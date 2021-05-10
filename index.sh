d70b73d61de576a57142c10e23844efa2f7152dc
root <wixancl@gmail.com>
2021-05-10 19:24:27 +0000

    2021_05_10_19_24_27

--- index.sh
+++ index.sh
@@ -28,33 +28,27 @@ echo -e "\e[92m  \e[0m"
 
 }
 
+clear
+
+
 presentacion
 
 
-echo -e "\e[96m Escriba el Nombre de la aplicacion \e[0m"
-echo -n " >> "
-read  app
-echo  $app
-echo -e "\e[92m  \e[0m"
-
-
-if [ ! -d $app ]
-then 
-     echo -e "\e[92m El directorio no existe \e[0m"
-     echo -e "\e[92m Se creara la aplicacion \e[0m"
-     composer create-project laravel/laravel $app
-else 
-     if [ "$(ls $app)" ]
-     then  
-         echo "el directorio tiene algo"
-     else 
-         echo "el directorio está vacío"
-     fi
-fi
-
-
-
-
-
-# composer create-project laravel/laravel example
-
+PS3="Selecciona una operación: " 
+select opt in instalar reiniciar salir; 
+do 
+    case $opt in 
+        instalar) 
+	    . installapp.sh 
+            ;;
+    	reiniciar)
+	    index.sh
+	    ;;
+        salir) 
+            break 
+            ;; 
+        *) 
+            echo "$REPLY no es una operación válida" 
+            ;; 
+    esac 
+done
