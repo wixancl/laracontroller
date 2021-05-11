9127111e9163b2a7ea9feadd9fec5ed4e092d731
root <wixancl@gmail.com>
2021-05-11 12:53:42 +0000

    2021_05_11_12_53_42

--- index.sh
+++ index.sh
@@ -28,18 +28,81 @@ echo -e "\e[92m  \e[0m"
 
 }
 
-clear
 
 
-presentacion
+instalarapp()
+{
+echo -e "\e[92m ################################################# \e[0m"
+echo -e "\e[93m # Instalacion de Aplicacion                     # \e[0m" 
+echo -e "\e[93m #                                               # \e[0m" 
+echo -e "\e[92m ################################################# \e[0m" 
+
+echo -e "\e[96m Escriba el Nombre de la aplicacion \e[0m" 
+echo -n " >> "
+read  app
+echo  $app
+echo -e "\e[92m  \e[0m"
+
+if [ ! -d $app ]
+then
+     echo -e "\e[92m El directorio no existe \e[0m"
+     echo -e "\e[92m Se creara la aplicacion \e[0m"
+     composer create-project laravel/laravel $app
+     echo -e "\e[92m La aplicacion ha sido creada \e[0m"
+     read -rsp $'Presione cualquier tecla o espere 5 segundos para continuar  \n' -n 1 -t 5;
+else
+     if [ "$(ls $app)" ]
+     then
+
+         echo -e "\e[92m El directorio no esta vacio \e[0m"
+         echo -e "\e[92m Si desea borrar la aplicacion presione Y en caso contrario N \e[0m"
+	 
+	 read opcionborraraplicacion
+	 if [ $opcionborraraplicacion = "Y" ]
+         then
+		 rm -rf $app
+	 elif [ $opcionborraraplicacion = "N" ]           
+	 then
+	 	 echo -e "\e[92m no se ha ejecutado ninguan accion  \e[0m"
+	 else
+	 	 echo -e "\e[92m no se ha ejecutado ninguan accion  \e[0m"
+         fi
+
+     else
+
+         echo -e "\e[92m El directorio no esta vacio \e[0m"
+         echo -e "\e[92m Si desea borrar la aplicacion presione Y en caso contrario N \e[0m"
+
+	 read opcionborraraplicacion
+
+	 if [ $opcionborraraplicacion = "Y" ]
+         then
+		 rm -rf $app
+	 elif [ $opcionborraraplicacion = "N" ]           
+	 then
+	 	 echo -e "\e[92m no se ha ejecutado ninguan accion  \e[0m"
+	 else
+	 	 echo -e "\e[92m no se ha ejecutado ninguan accion  \e[0m"
+         fi
+    fi
+fi
+
+}
+
+borraraplicacion()
+{
+rm -rf $app
+}
 
 
+menu()
+{
 PS3="Selecciona una operación: " 
 select opt in instalar reiniciar salir; 
 do 
     case $opt in 
         instalar) 
-	    . installapp.sh 
+	    instalarapp
             ;;
     	reiniciar)
 	    index.sh
@@ -52,3 +115,8 @@ do
             ;; 
     esac 
 done
+}
+
+clear
+presentacion
+menu
